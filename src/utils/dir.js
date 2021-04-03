8764ca1b5e4172efe98b5c5166f87681fdf77cf7
Carl Räfting <carl.rafting@gmail.com>
2021-04-03 10:14:40 +0200

    Update dir.js
    Keep track of directory depth

--- src/utils/dir.js
+++ src/utils/dir.js
@@ -5,17 +5,22 @@ import _path from 'path';
  * @typedef {Object} defaults
  * @property {boolean} recursive get directory content recursively
  * @property {boolean} clearResults when true, results set is cleared between method calls
+ * @property {boolean} maxDepth max depth to traverse directories
  * @property {boolean} filesOnly only get directory file contents
  * @property {boolean} dirsOnly only get a directorys directory contents
  **/
 const defaults = {
   recursive: true,
   clearResults: true,
+  maxDepth: 3,
   filesOnly: false,
   dirsOnly: false
 };
 
 let _refs = null;
+
+/** @typedef {Set<string>} results */
+const results = !_refs ? new Set() : _refs;
 
 /**
  * Add content of a directory to  a results set, recursive by default.
@@ -23,39 +28,37 @@ let _refs = null;
  * @returns {results} a results set of directory contents
  */
 export async function getDirContent(path = '.', options={ ...defaults }) {
-  /** @typedef {Set<string>} results */
-  const results = !_refs ? new Set() : _refs;
-  
   const dir = await fs.promises.opendir(path);
+  
+  let depth = 0; 
 
   const _options = {
     ...defaults,
     ...options
   };
 
+  if (depth >= _options.maxDepth) {
+    return results;
+  }
+
   for await (const dirent of dir) {
     const filePath = _path.join(path, dirent.name);
 
     console.log(dirent);
 
-    if (_options.recursive) {
-      if (dirent.isDirectory()) {
-        results.add(filePath);
-        _refs = results;
-        await getDirContent(filePath, _options);
-      }
+    if (_options.recursive && dirent.isDirectory()) {
+      depth+=1;
+      await getDirContent(filePath, _options);
     }
-
+    
     if (_options.filesOnly) {
       if (dirent.isFile()) {
         results.add(filePath);
       }
     }
-  }
 
-  // if (_options.clearResults) {
-  //   results.clear();
-  // }
+    results.add(filePath);
+  }
 
   return results;
 }
