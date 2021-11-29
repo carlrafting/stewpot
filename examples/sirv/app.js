34f1c97ccbb9b937d0e692e725502340f4f7f7d6
Carl Räfting <carl.rafting@gmail.com>
2021-11-29 16:32:13 +0100

    Change: Simplify sirv example.

--- examples/sirv/app.js
+++ examples/sirv/app.js
@@ -3,37 +3,21 @@ import sirv from 'sirv';
 
 router
   .add('root', 'get', '/', (request, response) => {
-    response.end(
-      `Check out <a href="/static/">/static/ for an example of serving static assets with sirv.</a>.`
-    );
+    
   })
-  .add('home', 'get', '/home/', (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(
-      JSON.stringify({
-        data: 'Welcome Home!',
-      })
-    );
-  });
 
 const servePublic = sirv('public', {
-  maxAge: 0, // 0 for development purposes
+  maxAge: 0, // 0 for development environment
   immutable: true
 });
 
 application(null, (request, response) => {
   try {
     servePublic(request, response, () => {
-      try {
-        return router.route(request, response);
-      } catch (error) {
-        console.error('router', error);
-        response.end(
-          JSON.stringify({
-            error: error.message,
-          })
-        );
-      }   
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(
+        `Check out /static/ for an <a href="/static/">example of serving static assets with sirv</a>.`
+      );
     });
   } catch (error) {
     console.log('servePublic', { error });
