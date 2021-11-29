45a78703fa6945d2ad1cfede95e8b9e76c825be6
Carl Räfting <carl.rafting@gmail.com>
2021-11-29 11:09:55 +0100

    Add nodemon with router example.

---
+++ examples/nodemon-with-router/app.js
@@ -0,0 +1,33 @@
+import application from 'stewpot/src/server/application.js';
+import * as router from 'stewpot/src/server/router.js';
+
+router
+  .add('root', 'get', '/', (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(
+      JSON.stringify({
+        data: 'Hello World!',
+      })
+    );
+  })
+  .add('home', 'get', '/home/', (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(
+      JSON.stringify({
+        data: 'Welcome Home!',
+      })
+    );
+  });
+
+application(null, (request, response) => {
+  try {
+    return router.route(request, response);
+  } catch (error) {
+    console.error(error);
+    response.end(
+      JSON.stringify({
+        error: error.message,
+      })
+    );
+  }
+}).run();
