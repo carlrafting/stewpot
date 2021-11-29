6c14fc5c2dadad7ed1118fd1e6b340aa65ecde27
Carl Räfting <carl.rafting@gmail.com>
2021-11-29 15:36:58 +0100

    Remove unused template.js file.

--- src/server/template.js
+++
@@ -1,6 +0,0 @@
-export function render(template, data={}, layout='default.html') {
-  if (!template || typeof template !== 'string') {
-    throw Error(`Template Render Error!`);
-  }
-  // TODO: detect if template is path or template string... 
-}
