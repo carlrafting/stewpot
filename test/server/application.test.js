8a8c5335830780574e782fb4faab895a3e0f0163
Carl Räfting <carl.rafting@gmail.com>
2021-11-29 15:42:45 +0100

    Add: Test cases for application.js.

--- test/server/application.test.js
+++ test/server/application.test.js
@@ -1,8 +1,56 @@
+import http from 'node:http';
 import application from '../../src/server/application.js';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
 
-const app = application();
-console.log('app.config', app.config);
-app.run();
+function makeRequest(options, callback) {
+  return http.request(options || {
+    host: 'localhost',
+    port: 8080,
+    method: 'GET'
+  }, (response) => {
+    callback && callback(response)
+  });
+}
 
-// const app_two = application({ port: 8080 });
-// app_two.run();
+test('is a function', () => {
+  assert.type(application, 'function');
+});
+
+test('has run method', () => {
+  const app = application();
+  assert.type(app.run, 'function');
+});
+
+test('successfully overrides default configuration', () => {
+  application({
+    server: {
+      port: 8000
+    }
+  }, (request, response) => {
+    console.log(request);
+    assert.ok(response.statusCode);
+    assert.equal(response.statusCode, 200);
+  }).run(() => {
+    request.end();
+  })
+  const request = makeRequest({
+    host: 'localhost',
+    port: 8000,
+    method: 'GET'
+  });
+});
+
+test('successfully listens on default port', () => {
+  const app = application(null, (request, response) => {
+    assert.equal(response.statusCode, 200);
+    response.end() && app.close();
+  });
+  app.run(() => {
+    request.end();
+  });
+  const request = makeRequest(null, (response) => {
+    assert.ok(response);
+    assert.equal(response.statusCode, 200);
+  });
+});
