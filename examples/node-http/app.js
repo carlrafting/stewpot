e5fb97bc92b2ea54ef94cdb3ee4929a7488958b2
Carl <carl.rafting@gmail.com>
2022-10-11 23:29:14 +0200

    fix node http example

--- examples/node-http/app.js
+++ examples/node-http/app.js
@@ -1,5 +1,5 @@
 import http from 'node:http';
-import router from '../../src/server/router.js';
+import router from 'stewpot/router';
 
 const controller = new AbortController();
 const host = '127.0.0.1';
@@ -7,9 +7,8 @@ const port = 80;
 const protocol = http !== undefined ? 'http://' : 'https://';
 
 const r = router()
-    .use('/', function mw(req, res, next) {
+    .use('/', function mw(_, res) {
         res.write('hello from mw \n');
-        next();
     })
     .get('/', function first(_, res) {
         res.end('hello world');
@@ -21,17 +20,7 @@ const r = router()
         res.end('hello with id');
     });
 
-function handler(req, res) {
-    const { pathname, method } = new URL(req.url, `http://${req.headers.host}`);
-
-    const matches = r.find(method, pathname);
-
-    console.log(matches);
-
-    res.end('Hello World!');
-}
-
-const server = http.createServer(handler);
+const server = http.createServer(r.handler);
 
 server.listen(
     {
@@ -48,26 +37,3 @@ server.listen(
         );
     }
 );
-
-function retry() {
-    console.log('Retrying...');
-    setTimeout(() => {
-        let p = port;
-        controller.abort();
-        server.listen((p += 1));
-    }, 1000);
-}
-
-server.on('error', (err) => {
-    //console.log(err);
-    if (err.code === 'EACCES') {
-        console.log(
-            `${err.message}. You don't have the right privileges to listen on port ${port}`
-        );
-        retry();
-    }
-    if (err.code === 'EADDRINUSE') {
-        console.log(`A server is already running at port ${port}...`);
-        retry();
-    }
-});
