36ac87c9c1f29ca74e84df0c75b57a1b00b52ae9
Carl <carl.rafting@gmail.com>
2022-10-12 21:35:35 +0200

    stewpot/router
    fix failing tests

--- src/server/router.test.js
+++ src/server/router.test.js
@@ -1,5 +1,11 @@
 import test from 'ava';
 import r from './router.js';
+import { methods } from './router.js';
+
+// test has to run serially due to sharing the router variable.
+// https://github.com/avajs/ava/blob/main/docs/08-common-pitfalls.md#sharing-variables-between-asynchronous-tests
+
+const empty = () => {};
 
 let router;
 
@@ -7,26 +13,42 @@ test.beforeEach(() => {
     router = r();
 });
 
-test('should add routes', (t) => {
-    t.assert(router.add('GET', 'foo', () => {}));
-    t.assert(router.add('POST', 'foo', () => {}));
+function create() {
+    return r();
+}
+
+test.serial('add - should add routes for all http methods', (t) => {
+    methods.map((method) => {
+        router.add(method, '/', empty);
+    });
+
+    t.is(router.routes.length, methods.length), 'has same length';
 });
 
-test('should accept lowercase value for method parameter', (t) => {
-    t.assert(router.add('get', 'root', () => {}));
-    t.assert(router.add('post', 'root', () => {}));
+test.serial('add - is chainable', (t) => {
+    const actual = router.add('get', '/', empty);
+    t.is(typeof actual, 'object');
+    t.not(typeof actual, 'undefined');
+    t.assert(actual !== undefined);
 });
 
-test('should not be able to add duplicate routes', (t) => {
-    t.assert(router.add('get', 'duplicate', () => {}));
-    t.assert(router.add('get', 'duplicate', () => {}));
+test.serial('add - should accept lowercase value for method parameter', (t) => {
+    ['get', 'post'].map((method) => router.add(method, '/', empty));
+    t.assert(router.routes.length > 0);
+    t.is(router.routes.length, 2);
+    router.routes.map((route) => {
+        t.assert(typeof route.method === 'string');
+        t.is(route.method, route.method.toLowerCase());
+    });
 });
 
-test('should find correct route', (t) => {
-    t.assert(router.add('get', '/', () => {}));
-    t.assert(router.add('get', '/foo', () => {}));
-    t.assert(router.find('get', '/'));
-    // console.log(router.inspect());
+test('find - should find correct route', (t) => {
+    const router = create();
+    router.add('get', '/', empty);
+    router.add('get', '/foo', empty);
+    const results = router.find('get', '/');
+    t.assert(typeof results.params === 'object');
+    t.assert(Array.isArray(results.handlers));
+    t.is(results.handlers.length, 1);
+    t.assert(typeof results.handlers[0] === 'function');
 });
-
-// test.run();
