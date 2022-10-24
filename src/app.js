2da04c4753045731bf13c7357649fe545d513c6b
Carl Räfting <carl.rafting@gmail.com>
2022-10-24 21:20:44 +0200

    stewpot/app
    cleanup. default_handler is already registered.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
 import stewpot from 'stewpot/app';
-import defaultHandler from './server/default_handler.js';
 
 const app = stewpot({ port: 80 });
-const { handler } = defaultHandler();
-
-app.use(handler);
 app.listen();
