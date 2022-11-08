6ad1228ba79c763e7c6b265a953a8d208a5ca42f
Carl Räfting <carl.rafting@gmail.com>
2022-11-08 14:35:59 +0100

    fix directory path

--- examples/router.js
+++ examples/router.js
@@ -1,7 +1,8 @@
 import stewpot from 'stewpot';
 import { toFileUrl, resolve } from 'path/mod.ts';
 
-const directory = "test/app";
+// paths are relative to examples directory
+const directory = "../test/app";
 const module = async () => {
   return (await import(
     toFileUrl(resolve(directory, 'examples/router.js'))
