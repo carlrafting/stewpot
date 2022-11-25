00bd004af9e2c68bf1c7031114dafde9e24cbc79
Carl Räfting <carl.rafting@gmail.com>
2022-11-25 15:57:59 +0100

    add nunjucks plugin

---
+++ packages/deno/examples/nunjucks/main.js
@@ -0,0 +1,27 @@
+import stewpot from "stewpot/stewpot.js";
+import { join } from "stewpot/deps.js";
+import nunjucksPlugin from "stewpot/plugins/nunjucks.js";
+
+function handler({ pathname, render }) {
+  if (pathname === "/") {
+    return async () => {
+      return await render("index", { data: { message: "Nunjucks" } });
+    };
+  }
+  if (pathname === "/string") {
+    return async () => {
+      return await render("<h1>Hello from {{ message }}!</h1>", { inline: true, data: { message: "Nunjucks" } });
+    };
+  }
+}
+
+if (import.meta.main) {
+  stewpot({
+    root: join(Deno.cwd(), "packages/deno/examples/nunjucks"),
+    handler,
+    plugins: [
+      nunjucksPlugin()
+    ],
+    templateFormat: "njk",
+  });
+}
---
+++ packages/deno/plugins/nunjucks.js
@@ -0,0 +1,26 @@
+import { nunjucks } from "../deps.js";
+
+export default () => {
+    const templateFormat = "njk";
+  
+    return function nunjucksPlugin({ state }) {
+      state.templateFormats.push(templateFormat);
+  
+      function configure(templateDir) {
+        return nunjucks.configure(templateDir);
+      }
+  
+      return {
+        type: "template",
+        name: "nunjucks",
+        templateFormat,
+        renderString(template, data) {
+          return nunjucks.renderString(template, data);
+        },
+        renderFile(templateDir, template, data) {
+          const env = configure(templateDir);
+          return env.render(template, data);
+        },
+      };
+    };
+  };
