7bb715598e240f1acb3132472c3290ce098988af
Carl Räfting <carl.rafting@gmail.com>
2023-09-01 21:32:51 +0200

    add vento plugin

---
+++ packages/deno/examples/vento/main.js
@@ -0,0 +1,41 @@
+import stewpot, { send } from "../../stewpot.js";
+import ventoPlugin from "../../plugins/vento.js";
+
+function handler({ pathname, render }) {
+  const date = new Date().toLocaleString();
+  if (pathname === "/") {
+    return async () => {
+      return send(
+        await render("index", {
+          data: { title: "Welcome to Vento!", message: "Vento", date },
+        }),
+      );
+    };
+  }
+  if (pathname === "/string") {
+    return async () => {
+      return send(
+        await render("<h1>Hello {{ title }}</h1>", {
+          inline: true,
+          data: { title: "Vento" },
+        }),
+      );
+    };
+  }
+}
+
+export function main() {
+  return stewpot({
+    root: "packages/deno/examples/vento",
+    handler,
+    plugins: [
+      ventoPlugin(),
+    ],
+    templateFormat: "vto",
+  });
+}
+
+if (import.meta.main) {
+  console.log(import.meta);
+  main();
+}
