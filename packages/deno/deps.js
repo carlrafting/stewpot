d0da3b6084f7908b41560cb3c0dde0bffb47caea
Carl Räfting <carl.rafting@gmail.com>
2023-09-01 22:10:54 +0200

    update std lib version

--- packages/deno/deps.js
+++ packages/deno/deps.js
@@ -4,16 +4,16 @@ export {
   join,
   resolve,
   toFileUrl,
-} from "https://deno.land/std@0.198.0/path/mod.ts";
-export { getCookies } from "https://deno.land/std@0.198.0/http/cookie.ts";
-export { parse } from "https://deno.land/std@0.198.0/flags/mod.ts";
+} from "https://deno.land/std@0.201.0/path/mod.ts";
+export { getCookies } from "https://deno.land/std@0.201.0/http/cookie.ts";
+export { parse } from "https://deno.land/std@0.201.0/flags/mod.ts";
 import {
   bold,
   brightBlue,
   brightGreen,
   dim,
   red,
-} from "https://deno.land/std@0.198.0/fmt/colors.ts";
+} from "https://deno.land/std@0.201.0/fmt/colors.ts";
 export const colors = {
   brightBlue,
   brightGreen,
@@ -24,15 +24,15 @@ export const colors = {
 export {
   errors,
   isHttpError,
-} from "https://deno.land/std@0.198.0/http/http_errors.ts";
+} from "https://deno.land/std@0.201.0/http/http_errors.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.198.0/http/http_status.ts";
+} from "https://deno.land/std@0.201.0/http/http_status.ts";
 export {
   serveDir,
   serveFile,
-} from "https://deno.land/std@0.198.0/http/file_server.ts";
+} from "https://deno.land/std@0.201.0/http/file_server.ts";
 export { Router } from "./lib/Router.js";
 export { default as stewpot } from "./stewpot.js";
 export { Eta } from "https://deno.land/x/eta@v3.0.3/src/index.ts";
