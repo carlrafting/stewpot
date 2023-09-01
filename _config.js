7f7bb75ff5a889525b5d5930db23e6e9d963071d
Carl Räfting <carl.rafting@gmail.com>
2023-09-01 21:12:21 +0200

    Update _config.js
     fix paths

--- _config.js
+++ _config.js
@@ -19,9 +19,9 @@ for (const [key, value] of Object.entries(meta)) {
 
 site.remoteFile("_includes/footer.html", "packages/deno/templates/footer.html");
 site.remoteFile("_includes/header.njk", "packages/deno/templates/header.html");
-site.remoteFile("css/code.css", "packages/deno/templates/code.css");
-site.remoteFile("css/global.css", "packages/deno/templates/global.css");
-site.remoteFile("css/basic.css", "packages/deno/templates/basic.css");
+site.remoteFile("css/code.css", "packages/deno/static/code.css");
+site.remoteFile("css/global.css", "packages/deno/static/global.css");
+site.remoteFile("css/basic.css", "packages/deno/static/basic.css");
 site.remoteFile("index.md", "readme.md");
 // site.copy('css');
 site.loadAssets([".css", ".html"]);
