ad4db6af8a54c974cb4b1f34dc72f68f942e4951
Carl Räfting <carl.rafting@gmail.com>
2023-09-11 15:48:05 +0200

    add server script for deno deploy

---
+++ www/server.js
@@ -0,0 +1,10 @@
+import Server from "lume/core/server.ts";
+
+const server = new Server({
+  port: 8000,
+  root: `${Deno.cwd()}/www/dist`,
+});
+
+server.start();
+
+console.log("Listening on http://localhost:8000");
