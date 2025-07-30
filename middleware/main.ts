6430ed0ede383aae61169aa4135ff80972f6117a
Carl Räfting <121248+carlrafting@users.noreply.github.com>
2025-07-30 11:37:57 +0200

    helpers
    add helper methods for manipulating middlware stack

--- middleware/main.ts
+++ middleware/main.ts
@@ -1,6 +1,7 @@
 export type NextHandler = () => Promise<Response> | Response
 export type Middleware = (request: Request, next: NextHandler) => Promise<Response> | Response;
 export type RequestHandler = (request: Request) => Promise<Response> | Response;
+export type Helper = (middleware: Middleware[], index: number, fn: Middleware) => Middleware[];
 
 export function compose(middlewares: Middleware[], handler: RequestHandler): RequestHandler {
     return middlewares.reduceRight<RequestHandler>(
@@ -8,3 +9,7 @@ export function compose(middlewares: Middleware[], handler: RequestHandler): Req
         handler
     );
 }
+
+export const insert: Helper = (middleware: Middleware[], index: number, fn: Middleware) => middleware.splice(index, 0, fn);
+export const replace: Helper = (middleware: Middleware[], index: number, fn: Middleware) => middleware.splice(index, 1, fn);
+export const remove: Helper = (middleware: Middleware[], index: number) => middleware.splice(index, 1);
