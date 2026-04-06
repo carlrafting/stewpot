4a917152172f09ab833ba3ce9a54684ce41a2478
Carl Räfting <mail@carlrafting.com>
2026-04-06 11:58:10 +0200

    middleware: cleanup debug logging

--- middleware/aiblock.ts
+++ middleware/aiblock.ts
@@ -13,13 +13,11 @@ export function aiblock(options?: Options): Middleware {
       "https://raw.githubusercontent.com/ai-robots-txt/ai.robots.txt/refs/heads/main/haproxy-block-ai-bots.txt",
     );
     const blocklist: string[] = (await fetchBlocklist.text()).split("\n");
-    console.log({ blocklist });
     const headers = new Headers(request.headers);
     const userAgent = new UserAgent(headers.get("user-agent") ?? "");
 
     for (const agent of blocklist) {
       if (userAgent.ua.includes(agent)) {
-        console.log("what!?", agent);
         const status = STATUS_CODE.Forbidden;
         const statusText = STATUS_TEXT[status];
         return new Response(statusText, {
