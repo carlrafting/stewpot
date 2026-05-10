4064b3dd50157e12f04e740cf1ee3ea580721953
Carl Räfting <mail@carlrafting.com>
2026-05-10 12:20:00 +0200

    [manage/http/response] - add overload to functions - create json and text response helpers
    Signed-off-by: Carl Räfting <mail@carlrafting.com>

--- manage/http/response.ts
+++ manage/http/response.ts
@@ -1,8 +1,32 @@
-export function html(body: BodyInit, headers: HeadersInit = []) {
+export function html(body: BodyInit): Response;
+export function html(body: BodyInit, options?: ResponseInit): Response {
+  const headers = new Headers();
+  headers.set("content-type", "text/html; charset=utf-8");
   return new Response(body, {
-    headers: {
-      "content-type": "text/html; charset=utf-8",
-      ...headers,
-    },
+    headers,
+    ...options,
+  });
+}
+
+type Data = Record<string, unknown> | string[] | number[];
+
+export function json(data: Data): Response;
+export function json(data: Data, options?: ResponseInit): Response {
+  const headers = new Headers();
+  headers.set("content-type", "application/json");
+  const stringified = JSON.stringify(data);
+  return new Response(stringified, {
+    headers,
+    ...options,
+  });
+}
+
+export function text(body: string): Response;
+export function text(body: string, options?: ResponseInit): Response {
+  const headers = new Headers();
+  headers.set("content-type", "text/plain");
+  return new Response(body, {
+    headers,
+    ...options,
   });
 }
