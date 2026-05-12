fe5b9052f2c16621a18afc199e455c3a219ee0f1
Carl Räfting <mail@carlrafting.com>
2026-05-12 07:49:15 +0200

    [manage] - add onListener for http server
    Signed-off-by: Carl Räfting <mail@carlrafting.com>

--- manage/http/server.ts
+++ manage/http/server.ts
@@ -1,17 +1,22 @@
-interface Options extends Deno.ServeTcpOptions {}
-
 export async function createServer(
   instance: Deno.ServeDefaultExport,
-  options: Options = {},
+  options: Deno.ServeTcpOptions = {},
 ) {
   const abortController = new AbortController();
   const signal = abortController.signal;
   const hostname = "localhost";
   const port = 8000;
+  const onListen: Deno.ServeTcpOptions["onListen"] = (localAddr) => {
+    console.log(
+      "[server]",
+      `Listening on ${localAddr.hostname}:${localAddr.port}`,
+    );
+  };
   const server = Deno.serve({
     hostname,
     port,
     signal,
+    onListen,
     ...options,
   }, instance.fetch);
   Deno.addSignalListener("SIGINT", () => {
@@ -23,4 +28,6 @@ export async function createServer(
     abortController.abort();
   });
   await server.finished;
+  server.shutdown();
+  console.log("server shutdown finished!");
 }
