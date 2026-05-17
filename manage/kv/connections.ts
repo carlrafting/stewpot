3953e500e7f0d45712985fa2d005f864aec97ff0
Carl Räfting <mail@carlrafting.com>
2026-05-17 13:58:20 +0200

    [manage] - add `createDirectory` to connections.ts
    Signed-off-by: Carl Räfting <mail@carlrafting.com>

--- manage/kv/connections.ts
+++ manage/kv/connections.ts
@@ -2,9 +2,16 @@ import type { Options } from "../main.ts";
 
 export type ConnectionKey = "sessions" | "kv";
 
+async function createDirectory(meta: ImportMeta): Promise<void> {
+  const recursive: boolean = true;
+  const url = new URL("database", meta.url);
+  await Deno.mkdir(url, { recursive });
+}
+
 export async function createConnections(options: Options) {
   const connections = new Map<ConnectionKey, Deno.Kv>();
   {
+    await createDirectory(options.meta);
     const sessions = await Deno.openKv(options?.sessions?.path);
     const kv = await Deno.openKv(options?.kv?.path);
     connections.set("sessions", sessions);
